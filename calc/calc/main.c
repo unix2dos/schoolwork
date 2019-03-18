6f4366b3168b5a1ed1c5d3fd1c73e6da6dee973f
liuwei <levonfly@gmail.com>
2019-03-18 17:23:17 +0800

    update calc

--- calc/calc/main.c
+++ calc/calc/main.c
@@ -8,30 +8,59 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    int a, b; char op;
-    while (scanf("%d%c%d", &a, &op, &b)) {
+void clean_buffer(void);
+
+int main(int argc, char * argv[]) {
+    
+    printf("please input calc formula:\n");
+    
+    while (1) {
+
+        float a, b; char op;
+        if (scanf("%f%c%f", &a, &op, &b) != 3){
+            if (getchar() == 'q') {
+                return 0;
+            } else {
+                printf("usage:\n"
+                "\t1. input: 5-2 or 5/2\n"
+                "\t2. input q to exit\n");
+            }
+            goto CLEAN;
+        }
+
         switch (op) {
             case '+':
-                printf("%d%c%d=%d\n", a, op, b, a+b);
+                printf("%.1f%c%.1f=%.1f\n", a, op, b, a+b);
                 break;
             case '-':
-                printf("%d%c%d=%d\n", a, op, b, a-b);
+                printf("%.1f%c%.1f=%.1f\n", a, op, b, a-b);
                 break;
             case '*':
-                printf("%d%c%d=%d\n", a, op, b, a*b);
+                printf("%.1f%c%.1f=%.1f\n", a, op, b, a*b);
                 break;
             case '/':
                 if (b == 0) {
                     printf("divisor can't be zero\n");
                     break;
                 }
-                printf("%d%c%d=%.1lf\n", a, op, b, a*1.0/b);
+                printf("%.1f%c%.1f=%.1f\n", a, op, b, a*1.0/b);
                 break;
             default:
                 printf("unsupport operator\n");
                 break;
         }
+    CLEAN:
+        clean_buffer();
     }
+
     return 0;
 }
+
+
+// clean input buffer
+void clean_buffer() {
+    while (getchar() != '\n') {
+        continue;
+    }
+}
+
